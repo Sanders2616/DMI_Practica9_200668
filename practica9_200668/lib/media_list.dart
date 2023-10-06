081db40e3753f77a3f106baeac242d5bf85353b7
Sanders2616 <sandra.aguilarsantos@utxicotepec.edu.mx>
2023-10-05 21:15:45 -0600

    first commit

---
+++ practica9_200668/lib/media_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:practica9_200668/common/HttpHandler.dart';
+import 'package:practica9_200668/model/Media.dart';
+import 'package:practica9_200668/media_list_item.dart';
+
+class MediaList extends StatefulWidget {
+  @override
+  _MediaListState createState() => _MediaListState();
+}
+
+class _MediaListState extends State<MediaList> {
+  List<Media> _media = [];
+  //List<Media> _media = new List.empty();
+  @override
+  void initState() {
+    super.initState();
+    loadMovies();
+  }
+
+  void loadMovies() async {
+    var movies = await HttpHandler().fetchMovies();
+    setState(() {
+      _media.addAll(movies);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: _media.length,
+        itemBuilder: (BuildContext context, int index) {
+          return MediaListItem(_media[index]);
+        },
+      ),
+    );
+  }
+}
---
+++ practica9_200668/lib/model/Media.dart
@@ -0,0 +1,43 @@
+import 'package:practica9_200668/common/Util.dart';
+
+class Media {
+  int id;
+  double voteAverage;
+  String title;
+  String posterPath;
+  String backdropPath;
+  String overview;
+  String releaseDate;
+  List<dynamic> genreIds;
+
+  String getPosterUrl() => getMediumPictureUrl(posterPath);
+  String getBackDropUrl() => getLargePictureUrl(backdropPath);
+
+  factory Media(Map jsonMap) {
+    try {
+      return new Media.deserialize(jsonMap);
+    } catch (ex) {
+      throw ex;
+    }
+  }
+
+  Media.deserialize(Map json)
+      : id = json["id"].toInt(),
+        voteAverage = json["vote_average"].toDouble(),
+        title = json["title"],
+        posterPath = json["poster_path"] ?? "",
+        backdropPath = json["backdrop_path"] ?? "",
+        overview = json["overview"],
+        releaseDate = json["release_date"],
+        genreIds = json["genre_ids"].toList();
+
+  // Media.deserialize(Map json)
+  //     : id = json["id"].toInt(),
+  //       voteAverage = json["vote_average"].toDouble(),
+  //       title = json["title"],
+  //       posterPath = json["poster_path"] ?? "",
+  //       backdropPath = json["backdrop_path"] ?? "",
+  //       overview = json["overview"],
+  //       releaseDate = json["release_date"], // Corrected field name
+  //       genreIds = List<int>.from(json["genre_ids"]);
+}
